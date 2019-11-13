eda8e5d0a82f42a839400df44f2629b879eded83
TimaRebesh <amitetoya@gmail.com>
2019-11-13 17:12:17 +0300

    stage2

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log("Server has been started...");
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs"
 });
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+async function start() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://trebesh:1q2w3e4r@cluster0-ijzuc.mongodb.net/todos",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+      }
+    );
+    app.listen(PORT, () => {
+      console.log("Server has been started...");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
